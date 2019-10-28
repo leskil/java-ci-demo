1285d63141463ca2b99c9b88ba6c0ca8e0d19b2b
Lasse Eskildsen <leskil99@gmail.com>
2019-10-28 09:51:37 +0100

    Spring Boot initial commit

---
+++ src/main/java/com/leskil/cidemo/CiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.leskil.cidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leskil/cidemo/CiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leskil.cidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
